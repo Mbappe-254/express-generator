a2fc5a23ac941b80d8f7e16d326c595146314758
ALBERT O MATARA <amatara@asu.edu>
2023-11-02 11:21:09 -0500

    authenicating users express part 2

--- models/user.js
+++ models/user.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose');
-const passportLocalMongoose = require('passport-local-mongoose');
+//const passportLocalMongoose = require('passport-local-mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
     admin: {
         type: Boolean,
         required: true
@@ -10,5 +19,5 @@ const userSchema = new Schema({
 
 });
 
-userSchema.plugin(passportLocalMongoose);
+//userSchema.plugin(passportLocalMongoose);
 module.exports = mongoose.model('User',userSchema);
